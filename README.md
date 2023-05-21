b998880be3c5b2e305563a0ba362dba149b3ee7e
Zef Anderson <zeferino@Zefs-MacBook-Pro.local>
2023-05-21 12:14:09 -0600

    delete user working

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Simple_User_Manager
+Create a Simple User Manager (no Angular). 
+One view should be the form that creates new users. 
+The form should create a user with the following attributes:
+Username, name, email address, age
+Your server should then assign a unique userId to this user upon creation.
+Post the newly created user to the server and save it to a file, then redirect the client to the user listing view.
+One view should be the user listing.
+t should contain a table which should list the username, name, email address, and age of all created users, as well as an edit and delete button/link for each user
+The edit link should take you to the edit view for that user
+The delete link should delete the user
+One view should be an edit view.
+This view should contain a form similar to the create user form but that is pre-populated with the users information, and allows for editing of the correct users information.
--- userCreate.js
+++ userCreate.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
@@ -14,28 +13,59 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 app.get('/userList', (req, res) => {
-    res.render('userList');
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        const users = JSON.parse(data);
+        res.render('userList', { users })
+    })
 })
-app.get('/users', (req, res) => {
-    const users = {
-        uniqueID: uuidv4(),
-        username: req.body.username,
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age
-    };
-    const jsonData = JSON.stringify(users);
-    const filePath = ''
-
+app.get('/edit', (req, res) => {
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        const users = JSON.parse(data);
+        let targetUser = users.filter(user => user.id === req.query.id);
+        res.render('edit', {targetUser})
+    })
 })
 
 app.post('/create', (req, res) => {
-    res.render('userList')
-    res.end(`Name: ${user.name}\n 
-            Email: ${user.email}
-            Username: ${user.username} 
-            Age: ${user.age} `)
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        let users;
+        try {
+            users = JSON.parse(data); 
+        } catch (parseErr) {
+            users = []; 
+        }
+        let newUser = {
+            uuid: uuidv4(),
+            name: req.body.name,
+            email: req.body.email,
+            username: req.body.username,
+            age: req.body.age
+        };
+        users.push(newUser);
+        let json = JSON.stringify(users);
+        fs.writeFile('./users.json', json, 'utf8', () => {
+            console.log('Updated');
+            req.body.username = '';
+            req.body.name = '';
+            req.body.email = '';
+            req.body.age = '';
+            res.render('userList', { users });
+        });
+    })
 });
+app.post('/edit', (req, res) => {
+    res.redirect('/edit')
+})
+app.post('/delete', (req, res) => {
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        const temp = JSON.parse(data);
+        const updatedUsers = temp.filter(user => user.uuid !== req.body.id);
+        const json = JSON.stringify(updatedUsers);
+        fs.writeFile('./users.json', json, 'utf8', () => {
+            res.redirect('/userList');
+        });
+    })
+})
 
 app.listen(9000, () => {
     console.log('listening on port 9000');
