1440bdfb5d5e51aa5d4c131d9de2792ddf27dc4c
Zef <bcdfezz@gmail.com>
2023-05-23 11:50:37 -0600

    doing final checks

--- userCreate.js
+++ userCreate.js
@@ -1,29 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 const app = express();
-app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 app.get('/userList', (req, res) => {
     fs.readFile('./users.json', 'utf8', (err, data) => {
-        const users = JSON.parse(data);
+        let users = JSON.parse(data);
         res.render('userList', { users })
     })
 })
 app.get('/edit', (req, res) => {
-    fs.readFile('./users.json', 'utf8', (err, data) => {
-        const users = JSON.parse(data);
-        let targetUser = users.filter(user => user.id === req.query.id);
-        res.render('edit', {targetUser})
-    })
+    res.render('edit', { currentUser })
 })
 
 app.post('/create', (req, res) => {
@@ -45,21 +42,41 @@ app.post('/create', (req, res) => {
         let json = JSON.stringify(users);
         fs.writeFile('./users.json', json, 'utf8', () => {
             console.log('Updated');
-            req.body.username = '';
-            req.body.name = '';
-            req.body.email = '';
-            req.body.age = '';
-            res.render('userList', { users });
+            res.redirect('/userList');
         });
     })
 });
 app.post('/edit', (req, res) => {
-    res.redirect('/edit')
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        const users = JSON.parse(data);
+        const userID = req.body.uuid;
+        const currentUser = users.find(user => user.uuid === userID)
+        // console.log(currentUser);
+        res.render('edit', { currentUser })
+    })
+})
+app.post('/update', (req, res) => {
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        const oldUsers = JSON.parse(data);
+        const targetUser = oldUsers.find(user => user.uuid === req.body.uuid);
+        console.log('before: ', targetUser);
+        targetUser.name = req.body.name;
+        targetUser.email = req.body.email;
+        targetUser.username = req.body.username;
+        targetUser.age = req.body.age;
+        console.log('after: ', targetUser);
+        const users = oldUsers.filter(user => user.uuid !== req.body.uuid);
+        users.push(targetUser);
+        const json = JSON.stringify(users);
+        fs.writeFile('./users.json', json, 'utf8', () => {
+            res.redirect('/userList');
+        });
+    }) 
 })
 app.post('/delete', (req, res) => {
     fs.readFile('./users.json', 'utf8', (err, data) => {
-        const temp = JSON.parse(data);
-        const updatedUsers = temp.filter(user => user.uuid !== req.body.id);
+        const users = JSON.parse(data);
+        const updatedUsers = users.filter(user => user.uuid !== req.body.uuid);
         const json = JSON.stringify(updatedUsers);
         fs.writeFile('./users.json', json, 'utf8', () => {
             res.redirect('/userList');
